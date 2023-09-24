3a329b4d872d026afadd6bf1a483dd84fa8dace8
Aoulad Lahcene Oussama <oussama@kreatinc.com>
2023-09-24 11:15:24 +0100

    fixed untracked files

--- database/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    DATABASE_HOST: "127.0.0.1",
-    DATABASE_PORT: "27017",
-    DATABASE_NAME: "covid19",
-    DATABASE_USER: "",
-    DATABASE_PASSWORD: "",
-    DATA_URL: "https://raw.githubusercontent.com/AyatANSSAIEN/Covid19/main/"
-};
